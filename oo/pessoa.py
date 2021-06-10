d6be7cf970bee1c1cf5c0768ee19f12980eefa84
tiagotardelli <tiagob.tardelli@gmail.com>
2021-06-09 22:45:49 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -3,12 +3,21 @@
 #Método, é uma função que pertence a uma classe. Sempre está relacionado a um objeto
 #self = em java e C# usamos this
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('João')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Tiago'
+    print(p.nome)
+    print(p.idade)
