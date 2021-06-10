53ec52d63bc7f63939f0bcce6f51689f2729a928
tiagotardelli <tiagob.tardelli@gmail.com>
2021-06-09 22:14:51 -0300

    Criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,14 @@
 #Classe é a base da orientacao objetos, ela é a forma que define como nossos objetos se comportam
 #Classe também é usado como sinônimo de tipo
-
+#Método, é uma função que pertence a uma classe. Sempre está relacionado a um objeto
+#self = em java e C# usamos this
 class Pessoa:
-    pass
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
