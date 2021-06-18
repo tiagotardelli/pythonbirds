cfd0754f54f487c0293f9f16be918061e76dd437
tiagotardelli <tiagob.tardelli@gmail.com>
2021-06-18 05:42:10 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,23 +1,24 @@
-#Classe é a base da orientacao objetos, ela é a forma que define como nossos objetos se comportam
-#Classe também é usado como sinônimo de tipo
-#Método, é uma função que pertence a uma classe. Sempre está relacionado a um objeto
-#self = em java e C# usamos this
+# Classe é a base da orientacao objetos, ela é a forma que define como nossos objetos se comportam
+# Classe também é usado como sinônimo de tipo
+# Método, é uma função que pertence a uma classe. Sempre está relacionado a um objeto
+# self = em java e C# usamos this
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
-
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('João')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Tiago'
-    print(p.nome)
-    print(p.idade)
+    luciano = Pessoa(nome='Luciano')
+    tiago = Pessoa(luciano, nome='Tiago')
+    print(Pessoa.cumprimentar(tiago))
+    print(id(tiago))
+    print(tiago.cumprimentar())
+    print(tiago.nome)
+    print(tiago.idade)
+    for filho in tiago.filhos:
+        print(filho.nome)
