29a45de4478423e28fe8fc421c3370887accea23
tiagotardelli <tiagob.tardelli@gmail.com>
2021-06-20 23:44:22 -0300

    Sobscrita de metodo de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -11,7 +11,8 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        # return f'Olá {id(self)}'
+        return f'Olá meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -21,45 +22,63 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+
 class Homem(Pessoa):
-    pass
+    def cumprimentar(self):
+        # cumprimentar_da_classe=self.cumprimentar() - Errado, gera recursão
+        # cumprimentar_da_classe=Pessoa.cumprimentar() - Abordagem ruim, tem que ficar mudando a classe
+        cumprimentar_da_classe=super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+
 
 class Mutante(Pessoa):
     olhos = 3
 
+
+def imprimir(list: []):
+    for item in list:
+        print(item)
+
+
 if __name__ == '__main__':
     luciano = Mutante(nome='Luciano')
-    #luciano = Homem(nome='Luciano')
+    # luciano = Homem(nome='Luciano')
     # luciano = Pessoa(nome='Luciano')
     tiago = Homem(luciano, nome='Tiago')
     # tiago = Pessoa(luciano, nome='Tiago')
-    print(Pessoa.cumprimentar(tiago))
-    print(id(tiago))
-    print(tiago.cumprimentar())
-    print(tiago.nome)
-    print(tiago.idade)
+    imprimir([
+        Pessoa.cumprimentar(tiago),
+        id(tiago),
+        tiago.cumprimentar(),
+        tiago.nome,
+        tiago.idade,
+    ])
+
     for filho in tiago.filhos:
         print(filho.nome)
     tiago.sobrenome = 'Tardelli'
-    print(tiago.sobrenome)
+    imprimir([tiago.sobrenome])
     del tiago.filhos
     luciano.olhos = 1
     del luciano.olhos
     print(tiago.__dict__)
     print(luciano.__dict__)
     # Pessoa.olhos = 3
-    print(Pessoa.olhos)
-    print(tiago.olhos)
-    print(luciano.olhos)
-    print(id(Pessoa.olhos), id(luciano.olhos), id(tiago.olhos))
-    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
-    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
+    imprimir([
+        Pessoa.olhos,
+        tiago.olhos,
+        luciano.olhos,
+        (id(Pessoa.olhos), id(luciano.olhos), id(tiago.olhos)),
+        (Pessoa.metodo_estatico(), luciano.metodo_estatico()),
+        (Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
+    ])
     pessoa = (Pessoa('Anonimo'))
-    print(isinstance(pessoa, Pessoa))
-    print(isinstance(pessoa, Homem))
-    print(isinstance(tiago, Pessoa))
-    print(isinstance(tiago, Homem))
-    print(luciano.olhos)
-
-
-
+    imprimir([
+        isinstance(pessoa, Pessoa),
+        isinstance(pessoa, Homem),
+        isinstance(tiago, Pessoa),
+        isinstance(tiago, Homem),
+        luciano.olhos,
+        tiago.cumprimentar(),
+        luciano.cumprimentar()
+    ])
