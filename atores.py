cb7b24c667ae267092f00c031c85fb133bcab60d
tiagotardelli <tiagob.tardelli@gmail.com>
2021-06-28 05:55:51 -0300

    Implementação do método de colisão

--- atores.py
+++ atores.py
@@ -52,8 +52,11 @@ class Ator():
         :param intervalo: Intervalo a ser considerado
         :return:
         """
-        pass
-
+        if self.status == ATIVO and outro_ator.status == ATIVO:
+            delta_x = abs(self.x - outro_ator.x)
+            delta_y = abs(self.y - outro_ator.y)
+            if delta_x <= intervalo and delta_y <= intervalo:
+                self.status = outro_ator.status = DESTRUIDO
 
 
 class Obstaculo(Ator):
@@ -118,7 +121,6 @@ class Passaro(Ator):
         :return: posição x, y
         """
         return 1, 1
-
 
     def lancar(self, angulo, tempo_de_lancamento):
         """
